ee2a56adc73563300f7f0c5783d851836bf72621
Wanda Antoine <wanda@thinkful.com>
2020-07-03 17:54:42 -0400

    updated Homepage passed on props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-// import Data from "./Data"
+import Data from "./Data"
 import LandingPage from './LandingPage'
 import HomePage from './HomePage'
 import AccountPage from './AccountPage'
@@ -10,38 +10,44 @@ import MyPostsPage from './MyPostsPage'
 import About from './About'
 import Recipes from './Recipes'
 import Forum from './Forum'
+import tabsProp from './Data';
 
 
 
 class App extends Component {
-  render(){
-  // const MyData = Data.map((tabsProp, key) =>
-  //   <Data tabsProp={tabsProp} key={tabsProp.id} />)
-  // console.log(MyData);
+  render() {
+    const MyData = Data.map((tabsProp, key) =>
+      <Route
+        exact path='/homepage'
+        render={props => <HomePage tabs={tabsProp} key={tabsProp.id} />}
+      />
+    )
+      console.log(tabsProp)
 
-  return (
-    <main className="App">
-      <BrowserRouter>
-        {/* <LandingPage /> */}
-        {/* <HomePage /> */}
-        {/* <About /> */}
-        <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/homepage' component={HomePage} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/search-recipes' component={Recipes} />
-          <Route exact path='/forum' component={Forum} />
-          <Route path='/my-recipes-page' component={MyRecipesPage} />
-          <Route path='/my-posts-page' component={MyPostsPage} />
-          <Route path='/account' component={AccountPage} />
+        return (
+      <main className="App">
+        <BrowserRouter>
+          {/* <LandingPage /> */}
+          {/* <HomePage /> */}
+          {/* <About /> */}
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            {/* <Route exact path='/homepage' component={HomePage} /> */}
+            {MyData}
+            <Route exact path='/about' component={About} />
+            <Route exact path='/search-recipes' component={Recipes} />
+            <Route exact path='/forum' component={Forum} />
+            <Route path='/my-recipes-page' component={MyRecipesPage} />
+            <Route path='/my-posts-page' component={MyPostsPage} />
+            <Route path='/account' component={AccountPage} />
 
-        </Switch>
-        {/* <AccountPage /> */}
-        {/* <MyRecipesPage />
+          </Switch>
+          {/* <AccountPage /> */}
+          {/* <MyRecipesPage />
           <MyPostsPage /> */}
-      </BrowserRouter>
-    </main>
-  );
-}
+        </BrowserRouter>
+      </main>
+    )
+  }
 }
 export default App;
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Data from "./Data"
+// import Data from "./Data"
 
 // import About from './About'
 // import Recipes from './Recipes'
@@ -27,8 +27,8 @@ import { Link } from 'react-router-dom';
 // ]
 
 class HomePage extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             // loading: false,
             results: [],
@@ -41,7 +41,7 @@ class HomePage extends Component {
         // console.log(myData);
 
 
-        // console.log(tabsProp)
+        console.log(this.props.tabs)
         return (
             <div>
                 <Navbar />
@@ -59,7 +59,7 @@ class HomePage extends Component {
                     <div className="icon-container">
                         <Link to="about"><img src="/images/about-image.jpg" alt="about-icon" /></Link>
                         <Link to="search-recipes"><img src="/images/recipe-image.jpg" alt="recipe-icon" /></Link>
-                        <Link to="forum"><img src="/images/forum-image2.jpg" alt="forum-icon" /></Link>
+                        <Link to="forum" tabs={this.props.tabs} key={this.props.tabs.id}><img src="/images/forum-image2.jpg" alt="forum-icon" /></Link>
                     </div>
 
                     {/* <About /> */}
