58358f2a6ecf535a706e24e99ee8033bd79effc8
Wanda Antoine <wanda@thinkful.com>
2020-07-10 03:39:29 -0400

    fixed login & register background

--- src/Login.js
+++ src/Login.js
@@ -1,164 +1,176 @@
-import React, { Component } from 'react'
-import config from './config'
-import { Link } from 'react-router-dom'
-
+import React, { Component } from "react";
+import config from "./config";
+import { Link } from "react-router-dom";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      params: {
+        loginUsername: "",
+        loginPassword: "",
+      },
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            params: {
-                loginUsername: '',
-                loginPassword: ''
-            }
-        };
+  formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    );
+    return queryItems.join("&");
+  }
+
+  validateUsername(inputEmail) {
+    let outputEmail = inputEmail;
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!inputEmail.match(mailformat)) {
+      outputEmail = "";
+    }
+    return outputEmail;
+  }
+
+  validatePassword(inputPassword) {
+    let outputPassword = inputPassword;
+    // at least one number, one lowercase and one uppercase letter
+    // at least eight characters that are letters, numbers or the underscore
+    let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
+    if (!inputPassword.match(passwordformat)) {
+      outputPassword = "";
+    }
+    return outputPassword;
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //create an object to store the search filters
+    const data = {};
+
+    //get all the from data from the form component
+    const formData = new FormData(e.target);
+
+    //for each of the keys in form data populate it with form value
+    for (let value of formData) {
+      data[value[0]] = value[1];
+    }
+    console.log(data);
+    let { loginUsername, loginPassword } = data;
+
+    //starting validation
+    //check username
+    if (this.validateUsername(loginUsername) === "") {
+      this.setState({
+        error: "username is not valid",
+      });
     }
 
-    formatQueryParams(params) {
-        const queryItems = Object.keys(params)
-            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-        return queryItems.join('&')
+    //check password
+    else if (this.validatePassword(loginPassword) === "") {
+      this.setState({
+        error: "password is not valid",
+      });
     }
+    //if validation is successful, make API call
+    else {
+      //assigning the object from the form data to params in the state
+      this.setState({
+        params: data,
+        error: null,
+      });
 
-    validateUsername(inputEmail) {
-        let outputEmail = inputEmail;
-        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (!inputEmail.match(mailformat)) {
-            outputEmail = ""
-        }
-        return outputEmail
+      //check if the state is populated with the search params data
+      console.log(this.state.params);
+
+      const searchURL = `${config.API_ENDPOINT}/login-page`;
+
+      const queryString = this.formatQueryParams(data);
+
+      //sent all the params to the final url
+      const url = searchURL + "?" + queryString;
+
+      console.log(url);
+
+      const options = {
+        method: "GET",
+        header: {
+          Authorization: "",
+          "Content-Type": "application/json",
+        },
+      };
+
+      //useing the url and paramters above make the api call
+      fetch(url, options)
+        // if the api returns data ...
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Something went wrong, please try again later.");
+          }
+          // ... convert it to json
+          return res.json();
+        })
+        // use the json api output
+        .then((data) => {
+          //check if there is meaningfull data
+          console.log(data);
+          // check if there are no results
+          if (data.totalItems === 0) {
+            throw new Error("No user found");
+          }
+        })
+        .catch((err) => {
+          // this.setState({
+          //     error: err.message
+          // })
+        });
     }
+  };
 
-
-    validatePassword(inputPassword) {
-        let outputPassword = inputPassword;
-        // at least one number, one lowercase and one uppercase letter
-        // at least eight characters that are letters, numbers or the underscore
-        let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
-        if (!inputPassword.match(passwordformat)) {
-            outputPassword = ""
-        }
-        return outputPassword
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        //create an object to store the search filters
-        const data = {}
-
-        //get all the from data from the form component
-        const formData = new FormData(e.target)
-
-        //for each of the keys in form data populate it with form value
-        for (let value of formData) {
-            data[value[0]] = value[1]
-        }
-        console.log(data)
-        let { loginUsername, loginPassword } = data
-
-        //starting validation
-        //check username
-        if (this.validateUsername(loginUsername) === '') {
-            this.setState({
-                error: 'username is not valid'
-            })
-        }
-
-        //check password
-        else if (this.validatePassword(loginPassword) === '') {
-            this.setState({
-                error: 'password is not valid'
-            })
-        } 
-        //if validation is successful, make API call
-        else {
-
-            //assigning the object from the form data to params in the state
-            this.setState({
-                params: data,
-                error: null
-            })
-
-            //check if the state is populated with the search params data
-            console.log(this.state.params)
-
-            const searchURL = `${config.API_ENDPOINT}/login-page`
-
-            const queryString = this.formatQueryParams(data)
-
-            //sent all the params to the final url
-            const url = searchURL + '?' + queryString
-
-            console.log(url)
-
-            const options = {
-                method: 'GET',
-                header: {
-                    "Authorization": "",
-                    "Content-Type": "application/json"
-                }
-            }
-
-            //useing the url and paramters above make the api call
-            fetch(url, options)
-
-                // if the api returns data ...
-                .then(res => {
-                    if (!res.ok) {
-                        throw new Error('Something went wrong, please try again later.')
-                    }
-                    // ... convert it to json
-                    return res.json()
-                })
-                // use the json api output
-                .then(data => {
-
-                    //check if there is meaningfull data
-                    console.log(data);
-                    // check if there are no results
-                    if (data.totalItems === 0) {
-                        throw new Error('No user found')
-                    }
-
-                })
-                .catch(err => {
-                    // this.setState({
-                    //     error: err.message
-                    // })
-                })
-        }
-    }
-
-    render() {
-        const errorMessage = this.state.error ? <p className="error-message">{this.state.error}</p> : false
-        return (
-            <section className="login-component">
-                <Link to='/forum'>forum</Link>
-                <h1 className="title">Everything Keto</h1>
-                <h2 className="subtitle">Log in to enter Ketosis!</h2>
-                <div className="form-div">
-                    <form className="login-form" onSubmit={this.handleSubmit}>
-                        {errorMessage}
-                        <label htmlFor="username">Username:</label>
-                        <input className="login-input" type="text" name="loginUsername" placeholder="my.username@ymail.com" required />
-                        <label htmlFor="password">Password:</label>
-                        <input className="login-input" type="password" name="loginPassword" placeholder="password" required />
-                        <button type="submit">Login</button>
-                    </form>
-
-                </div>
-                <div className="footer">
-                    <p>Don't have an account yet?</p>
-                    <a href="#register-component" className="register-link">
-                        <span>Sign up here</span>
-                    </a>
-                </div>
-            </section>
-        )
-    }
-
+  render() {
+    const errorMessage = this.state.error ? (
+      <p className="error-message">{this.state.error}</p>
+    ) : (
+      false
+    );
+    return (
+      <section className="login-component">
+        <div className="login-div">
+          <Link to="/forum">forum</Link>
+          <h1 className="title">Everything Keto</h1>
+          <h2 className="subtitle">Log in to enter Ketosis!</h2>
+          <div className="form-div">
+            <form className="login-form" onSubmit={this.handleSubmit}>
+              {errorMessage}
+              <label htmlFor="username">Username:</label>
+              <input
+                className="login-input"
+                type="text"
+                name="loginUsername"
+                placeholder="my.username@ymail.com"
+                required
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                className="login-input"
+                type="password"
+                name="loginPassword"
+                placeholder="password"
+                required
+              />
+              <button type="submit" className="login-button">
+                Login
+              </button>
+            </form>
+          </div>
+          <div className="footer">
+            <p>Don't have an account yet?</p>
+            <a href="#register-component" className="register-link">
+              <span>Sign up here</span>
+            </a>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Login;
--- src/Register.js
+++ src/Register.js
@@ -1,153 +1,166 @@
-import React, { Component } from 'react'
-import config from './config'
+import React, { Component } from "react";
+import config from "./config";
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      params: {
+        registerUsername: "",
+        registerPassword: "",
+      },
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            params: {
-                registerUsername: '',
-                registerPassword: ''
-            }
-        };
+  formatQueryParams(params) {
+    const queryItems = Object.keys(params).map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    );
+    return queryItems.join("&");
+  }
+
+  validateUsername(inputEmail) {
+    let outputEmail = inputEmail;
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!inputEmail.match(mailformat)) {
+      outputEmail = "";
     }
+    return outputEmail;
+  }
 
-    formatQueryParams(params) {
-        const queryItems = Object.keys(params)
-            .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-        return queryItems.join('&')
+  validatePassword(inputPassword) {
+    let outputPassword = inputPassword;
+    // at least one number, one lowercase and one uppercase letter
+    // at least eight characters that are letters, numbers or the underscore
+    let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
+    if (!inputPassword.match(passwordformat)) {
+      outputPassword = "";
     }
+    return outputPassword;
+  }
 
-    validateUsername(inputEmail) {
-        let outputEmail = inputEmail;
-        let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (!inputEmail.match(mailformat)) {
-            outputEmail = ""
-        }
-        return outputEmail
+  handleSubmit = (e) => {
+    e.preventDefault();
+    //create an object to store the search filters
+    const data = {};
+
+    //get all the from data from the form component
+    const formData = new FormData(e.target);
+
+    //for each of the keys in form data populate it with form value
+    for (let value of formData) {
+      data[value[0]] = value[1];
     }
-
-    validatePassword(inputPassword) {
-        let outputPassword = inputPassword;
-        // at least one number, one lowercase and one uppercase letter
-        // at least eight characters that are letters, numbers or the underscore
-        let passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{8,}$/;
-        if (!inputPassword.match(passwordformat)) {
-            outputPassword = ""
-        }
-        return outputPassword
+    console.log(data);
+    let { registerUsername, registerPassword } = data;
+    //starting validation
+    //validate username
+    if (this.validateUsername(registerUsername) === "") {
+      this.setState({
+        error: "username must be an email address",
+      });
     }
-
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        //create an object to store the search filters
-        const data = {}
-
-        //get all the from data from the form component
-        const formData = new FormData(e.target)
-
-        //for each of the keys in form data populate it with form value
-        for (let value of formData) {
-            data[value[0]] = value[1]
-        }
-        console.log(data)
-        let { registerUsername, registerPassword } = data
-        //starting validation
-        //validate username
-        if (this.validateUsername(registerUsername) === '') {
-            this.setState({
-                error: 'username must be an email address'
-            })
-        }
-        //validate password
-        else if (this.validatePassword(registerPassword) === '') {
-            this.setState({
-                error: 'password is not valid'
-            })
-        }
-        //if validation successful, make API call
-        else {
-
-            //assigning the object from the form data to params in the state
-            this.setState({
-                params: data,
-                error: null
-            })
-
-            //check if the state is populated with the search params data
-            console.log(this.state.params)
-
-            const searchURL = `${config.API_ENDPOINT}/registration-page`
-
-            const queryString = this.formatQueryParams(data)
-
-            //sent all the params to the final url
-            const url = searchURL + '?' + queryString
-            console.log(url)
-
-            //define the API call parameters
-            const options = {
-                method: 'POST',
-                header: {
-                    "Authorization": "",
-                    "Content-Type": "application/json"
-                }
-            }
-
-            //using the url and paramters above make the api call
-            fetch(url, options)
-
-                // if the api returns data ...
-                .then(res => {
-                    if (!res.ok) {
-                        throw new Error('Something went wrong, please try again later.')
-                    }
-                    // ... convert it to json
-                    return res.json()
-                })
-                // use the json api output
-                .then(data => {
-
-                    //check if there is meaningfull data
-                    console.log(data);
-                    // check if there are no results
-                    if (data.totalItems === 0) {
-                        throw new Error('No data found')
-                    }
-
-                })
-                .catch(err => {
-                    // this.setState({
-                    //     error: err.message
-                    // })
-                })
-        }
-
+    //validate password
+    else if (this.validatePassword(registerPassword) === "") {
+      this.setState({
+        error: "password is not valid",
+      });
     }
+    //if validation successful, make API call
+    else {
+      //assigning the object from the form data to params in the state
+      this.setState({
+        params: data,
+        error: null,
+      });
 
-    render() {
-        const errorMessage = this.state.error ? <p className="error-message">{this.state.error}</p> : false
-        return (
-            <section className="register-component">
-                <h1 className="title">Everything Keto</h1>
-                <h2 className="subtitle">Create your account!</h2>
-                <div className="form-div">
-                    <form className="register-form" onSubmit={this.handleSubmit}>
-                        {errorMessage}
-                        <label htmlFor="enter-username">Enter Username:</label>
-                        <input className="register-input" type="text" name="registerUsername" placeholder="my.username@ymail.com" required />
-                        <label htmlFor="enter-password">Enter Password:</label>
-                        <input className="register-input" type="password" name="registerPassword" placeholder="password" required />
-                        <button type="submit">Register</button>
-                    </form>
-                </div>
-            </section>
-        )
+      //check if the state is populated with the search params data
+      console.log(this.state.params);
+
+      const searchURL = `${config.API_ENDPOINT}/registration-page`;
+
+      const queryString = this.formatQueryParams(data);
+
+      //sent all the params to the final url
+      const url = searchURL + "?" + queryString;
+      console.log(url);
+
+      //define the API call parameters
+      const options = {
+        method: "POST",
+        header: {
+          Authorization: "",
+          "Content-Type": "application/json",
+        },
+      };
+
+      //using the url and paramters above make the api call
+      fetch(url, options)
+        // if the api returns data ...
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Something went wrong, please try again later.");
+          }
+          // ... convert it to json
+          return res.json();
+        })
+        // use the json api output
+        .then((data) => {
+          //check if there is meaningfull data
+          console.log(data);
+          // check if there are no results
+          if (data.totalItems === 0) {
+            throw new Error("No data found");
+          }
+        })
+        .catch((err) => {
+          // this.setState({
+          //     error: err.message
+          // })
+        });
     }
+  };
 
+  render() {
+    const errorMessage = this.state.error ? (
+      <p className="error-message">{this.state.error}</p>
+    ) : (
+      false
+    );
+    return (
+      <section className="register-component">
+        <div className="login-div">
+          <h1 className="title">Everything Keto</h1>
+          <h2 className="subtitle">Create your account!</h2>
+          <div className="form-div">
+            <form className="register-form" onSubmit={this.handleSubmit}>
+              {errorMessage}
+              <label htmlFor="enter-username">Enter Username:</label>
+              <input
+                className="register-input"
+                type="text"
+                name="registerUsername"
+                placeholder="my.username@ymail.com"
+                required
+              />
+              <label htmlFor="enter-password">Enter Password:</label>
+              <input
+                className="register-input"
+                type="password"
+                name="registerPassword"
+                placeholder="password"
+                required
+              />
+              <button type="submit" className="login-button">
+                Register
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Register;
